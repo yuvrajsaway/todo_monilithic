9fea74321746c73fd44e5f46c556793437d58df9
Yuvraj Saway <Yuvrajs@jayanita.com>
2025-11-13 16:06:20 +0530

    variable added

--- environment/dev/variables.tf
+++ environment/dev/variables.tf
@@ -50,8 +50,22 @@ variable "virtual_network" {
 }
 
 variable "nic" {
-  
+  type = map(object({
+    nic_name = string
+    location = string
+    rg_name  = string
+    subnet_name = string
+    virtual_network_name = string
+    tags     = optional(map(string))
+    ip_configuration = map(object({
+      name                          = string
+      # subnet_id                     = string
+      private_ip_address_allocation = string
+
+    }))
+  }))
 }
+
 
 # variable "linux_virtual_machine" {
 #   type = map(object({
